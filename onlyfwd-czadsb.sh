977c49dc0cadd4e1c1e4de2e8f82ab9aebc4cbf8
Tydyt <tydyt@webser1.slapnet.cz>
2024-09-08 17:56:40 +0200

    Fix not mlat files

--- onlyfwd-czadsb.sh
+++ onlyfwd-czadsb.sh
@@ -17,24 +17,35 @@ ARCH=$(dpkg --print-architecture)
 echo
 echo "Detekovan system: ${PRETTY_NAME} - ${ARCH}"
 echo
-# Stahni deb balicky pro konkretni architekturu
-WGET_URL="${URL_DEB}/${VERSION_CODENAME}/mlat-client_${MLAT_V}_${ARCH}.deb"
-echo "* Ztahuji ${WGET_URL}"
-wget -nv ${WGET_URL}
 
-WGET_URL="${URL_DEB}/${VERSION_CODENAME}/${ARCH}/mlat-client-dbgsym_${MLAT_V}_${ARCH}.deb"
-echo "* Ztahuji ${WGET_URL}"
-wget -nv ${WGET_URL}
-
-echo
-echo "* Instaluji mlat klienta"
-$SUDO dpkg -i mlat-client*.deb
-
+# Nainstaluj ADSBfwd a teprve pote mlat
 echo
 echo "* Stahuji a spoustim instalaci ADSBfwd"
 wget -q ${URL_FWD} -O /tmp/install.tmp
 . /tmp/install.tmp
 rm -f /tmp/install.tmp
+
+# Uklid stare instalacni balicky
+$SUDO rm -f ./mlat-client*.deb
+# Stahni deb balicky pro konkretni architekturu
+WGET_URL="${URL_DEB}/${VERSION_CODENAME}/mlat-client_${MLAT_V}_${ARCH}.deb"
+echo "* Ztahuji ${WGET_URL}"
+wget -nv ${WGET_URL}
+
+WGET_URL="${URL_DEB}/${VERSION_CODENAME}/mlat-client-dbgsym_${MLAT_V}_${ARCH}.deb"
+echo "* Ztahuji ${WGET_URL}"
+wget -nv ${WGET_URL}
+
+if [ -f ./mlat-client_${MLAT_V}_${ARCH}.deb ] && [ -f ./mlat-client-dbgsym_${MLAT_V}_${ARCH}.deb ]; then
+    echo
+    echo "* Instaluji mlat klienta"
+    $SUDO dpkg -i mlat-client*.deb
+else
+    echo
+    echo "* ERROR:"
+    echo "Pro Debian ${VERSION_CODENAME} ${ARCH} neni verze ${MLAT_V} k dispozici."
+    echo "Prosim kontaktujte z touto informaci autora skriptu."
+fi
 
 echo
 echo "Je nutne nakonfigurovat:"
